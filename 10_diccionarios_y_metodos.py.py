896d01df4792bfb6ded39b0be4653d8ab3c633e0
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-04 17:48:58 -0300

    Add files via upload

---
+++ 10_diccionarios_y_metodos.py.py
@@ -0,0 +1,6 @@
+persona = {" nombre": "juan", "edad": 25, "ciudad": "madrid"}
+print(persona.keys())
+print(persona.values())
+print(persona.items())
+persona.update({"profesion": "ingeniero"})
+print(persona)
