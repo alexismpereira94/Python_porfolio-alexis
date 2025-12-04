01d13487dcb060205ba507294c74bd5c5535f6d1
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-04 17:50:16 -0300

    Add files via upload

---
+++ 11_conjuntos_operaciones.py.py
@@ -0,0 +1,10 @@
+conjunto1 = {1, 2, 3}
+conjunto2 = {3, 4, 5}
+union = conjunto1 | conjunto2
+print(union)
+interseccion= conjunto1 & conjunto2
+print(interseccion)
+diferencia = conjunto1 - conjunto2
+print(diferencia)
+diferencia_simetrica= conjunto1 ^ conjunto2
+print(diferencia_simetrica)
