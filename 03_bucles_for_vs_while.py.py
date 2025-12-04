757e82246363402399cbbe4b5d62d6a7dbf4e36c
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-04 17:35:02 -0300

    Add files via upload

---
+++ 03_bucles_for_vs_while.py.py
@@ -0,0 +1,10 @@
+print( "numero del 1 al 5 multiplicados por 2 en bucle for:" )
+for numero in range(1, 6 ):
+    print( numero * 2)
+    
+    
+print("\nNumeros del 1 al 5 multiplicados por 2 con bulce while" )
+contador=1
+while contador <=5:
+    print( contador * 2)
+    contador+=1
