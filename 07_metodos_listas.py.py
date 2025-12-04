6c8936d2738b7cac2693b6b94f298d4b4035fb99
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-04 17:40:13 -0300

    Add files via upload

---
+++ 07_metodos_listas.py.py
@@ -0,0 +1,19 @@
+frutas = ["manzana", "banana", "naranja"]
+frutas.append("pera")
+print(frutas)
+
+frutas.insert(1,"uva")
+print( frutas)
+
+frutas.remove("banana")
+print(frutas)
+
+fruta_eliminada= frutas.pop(2)
+print(frutas)
+print(fruta_eliminada)
+
+frutas.sort()
+print(frutas)
+
+frutas.reverse()
+print(frutas)
