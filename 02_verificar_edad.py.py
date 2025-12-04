75239ea2423222267735ec10d399b87420fc0fa2
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-04 17:30:38 -0300

    Add files via upload
    es mi primer codigo "complejo"

---
+++ 02_verificar_edad.py.py
@@ -0,0 +1,12 @@
+edad=20
+if edad < 18:
+    print( " eres menor de edad" )
+elif edad >= 18 and edad < 60:
+        print( " ers un adulto" )
+elif edad > 15:
+            print( "mayor de 15" )
+elif edad == 60:
+                print( "feliz cumple años ")
+else:
+                    print( "usted es adulto mayor")
+                    
