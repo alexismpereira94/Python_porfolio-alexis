ab8ae243fbc9ec8f55ec3aef29ddac9cf54d8fee
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-04 17:51:43 -0300

    Add files via upload

---
+++ 12_metodos_conjuntos.py.py
@@ -0,0 +1,9 @@
+frutas = {"manzana", "banana", "naranja"}
+frutas.add("pera")
+print(frutas)
+frutas.remove("banana")
+print (frutas)
+frutas.discard("uva")
+print(frutas)
+frutas.clear()
+print(frutas)
