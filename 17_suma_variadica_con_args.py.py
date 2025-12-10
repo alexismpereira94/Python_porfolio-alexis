c22428d734f7c5eb5ae89a45b757772812ae55e4
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-10 18:30:52 -0300

    Add files via upload

---
+++ 17_suma_variadica_con_args.py.py
@@ -0,0 +1,9 @@
+def suma_variable(*numeros):
+    total= 0
+    for numero in numeros:
+        total+= numero
+        return total
+
+print (suma_variable(1, 2, 3,))
+print(suma_variable(4, 5, 6, 7))
+    
