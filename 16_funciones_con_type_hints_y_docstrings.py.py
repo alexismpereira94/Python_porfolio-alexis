82bb43ac95fe207df68bc53f2fb6b542d379a21c
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-10 18:25:28 -0300

    Add files via upload

---
+++ 16_funciones_con_type_hints_y_docstrings.py.py
@@ -0,0 +1,12 @@
+def area_rectangulo(base, altura):
+    """
+     calcula el area de un rectangulo
+     args:
+         base(float): La base del rectangulo.
+         altura8float): la altura del rectangulo-
+    returns:
+        float: el area del rectangulo.
+    """
+    return base * altura
+     
+
