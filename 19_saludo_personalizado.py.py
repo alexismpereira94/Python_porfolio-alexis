0937a5c4e8f71c04b1225e9ade1e4c7aac123c62
Alexis Pereira <alexismpereira.94@gmail.com>
2025-12-15 18:18:59 -0300

    Add files via upload

---
+++ 19_saludo_personalizado.py.py
@@ -0,0 +1,6 @@
+nombre= input("ingrresa tu nombre: ")
+apellido= input("ingresa tu apellido: ")
+edad= input("ingresa tu edad: ")
+print("hola",  nombre, apellido, "!")
+print("Tenes",  edad, "años", "!")
+
